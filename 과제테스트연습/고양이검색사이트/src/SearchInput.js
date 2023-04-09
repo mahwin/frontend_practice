89d8fa382a2d246f4422ab8b04561cea1d9fdeb8
youseock <mahwin7085@gmail.com>
2023-04-09 19:25:07 +0900

    미디어 커리 이용한 다크 모드 구현

--- 과제테스트연습/고양이검색사이트/src/SearchInput.js
+++ 과제테스트연습/고양이검색사이트/src/SearchInput.js
@@ -2,19 +2,42 @@ const TEMPLATE = '<input type="text">';
 
 class SearchInput {
   constructor({ $target, onSearch }) {
+    const $header = document.createElement("header");
+    this.$header = $header;
+
+    const $themeInput = document.createElement("input");
+    this.$themeInput = $themeInput;
+    $themeInput.type = "checkbox";
+
+    $themeInput.addEventListener("click", () => {
+      const theme = window.matchMedia("(prefers-color-scheme:dark)").matches;
+      if (theme && !$themeInput.checked) {
+        //다크
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+      } else if (theme && $themeInput.checked) {
+        //라이트
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+      }
+    });
+
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.!";
 
     $searchInput.className = "SearchInput";
-    $target.appendChild($searchInput);
+
     $searchInput.addEventListener("keyup", (e) => {
       if (e.code === "Enter") {
         onSearch(e.target.value);
       }
     });
 
-    console.log("SearchInput created.", this);
+    this.$header.appendChild(this.$themeInput);
+    this.$header.appendChild(this.$searchInput);
+
+    $target.appendChild(this.$header);
   }
   render() {}
 }
