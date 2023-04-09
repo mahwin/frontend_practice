b8d795051d85d66ebcb085cca7515a30f6e9d02a
youseock <mahwin7085@gmail.com>
2023-04-09 17:43:14 +0900

    App js 하위에서  결과 보여주는 컴포넌트 생성

--- 과제테스트연습/고양이검색사이트/src/App.js
+++ 과제테스트연습/고양이검색사이트/src/App.js
@@ -1,3 +1,168 @@
+const apiData = {
+  data: [
+    {
+      id: "dOqenzV1K",
+      url: "https://cdn2.thecatapi.com/images/dOqenzV1K.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "k5Vrnaz_Q",
+      url: "https://cdn2.thecatapi.com/images/k5Vrnaz_Q.jpg",
+      name: "Siamese / 샴 고양이",
+    },
+    {
+      id: "ai6Jps4sx",
+      url: "https://cdn2.thecatapi.com/images/ai6Jps4sx.jpg",
+      name: "Siamese / 샴 고양이",
+    },
+    {
+      id: "Dqtad3dGZ",
+      url: "https://cdn2.thecatapi.com/images/Dqtad3dGZ.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "35ayLP9Ql",
+      url: "https://cdn2.thecatapi.com/images/35ayLP9Ql.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "rBuGgH_Wj",
+      url: "https://cdn2.thecatapi.com/images/rBuGgH_Wj.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "5hmYjVhib",
+      url: "https://cdn2.thecatapi.com/images/5hmYjVhib.jpg",
+      name: "European Burmese / 유러피안 버마 고양이",
+    },
+    {
+      id: "EXe1GhbrN",
+      url: "https://cdn2.thecatapi.com/images/EXe1GhbrN.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "KUcr7eFlM",
+      url: "https://cdn2.thecatapi.com/images/KUcr7eFlM.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "-ZBBqoWNQ",
+      url: "https://cdn2.thecatapi.com/images/-ZBBqoWNQ.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "IFLjrNmc3",
+      url: "https://cdn2.thecatapi.com/images/IFLjrNmc3.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "35ayLP9Ql",
+      url: "https://cdn2.thecatapi.com/images/35ayLP9Ql.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "a_j82joTy",
+      url: "https://cdn2.thecatapi.com/images/a_j82joTy.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "YOjBThApG",
+      url: "https://cdn2.thecatapi.com/images/YOjBThApG.jpg",
+      name: "European Burmese / 유러피안 버마 고양이",
+    },
+    {
+      id: "Mdr6QqID0",
+      url: "https://cdn2.thecatapi.com/images/Mdr6QqID0.jpg",
+      name: "Burmese / 버마 고양이",
+    },
+    {
+      id: "PyIM3PKjl",
+      url: "https://cdn2.thecatapi.com/images/PyIM3PKjl.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "Dqtad3dGZ",
+      url: "https://cdn2.thecatapi.com/images/Dqtad3dGZ.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "7afi4uNFM",
+      url: "https://cdn2.thecatapi.com/images/7afi4uNFM.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "neW1xzZv4",
+      url: "https://cdn2.thecatapi.com/images/neW1xzZv4.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "a_j82joTy",
+      url: "https://cdn2.thecatapi.com/images/a_j82joTy.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "uvyjW5KIG",
+      url: "https://cdn2.thecatapi.com/images/uvyjW5KIG.jpg",
+      name: "European Burmese / 유러피안 버마 고양이",
+    },
+    {
+      id: "06dgGmEOV",
+      url: "https://cdn2.thecatapi.com/images/06dgGmEOV.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "f0UR7b5nA",
+      url: "https://cdn2.thecatapi.com/images/f0UR7b5nA.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "rWr-g-QVX",
+      url: "https://cdn2.thecatapi.com/images/rWr-g-QVX.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "KCJeb66J2",
+      url: "https://cdn2.thecatapi.com/images/KCJeb66J2.jpg",
+      name: "Siamese / 샴 고양이",
+    },
+    {
+      id: "-ZBBqoWNQ",
+      url: "https://cdn2.thecatapi.com/images/-ZBBqoWNQ.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "JR48AEqts",
+      url: "https://cdn2.thecatapi.com/images/JR48AEqts.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "2JgKj4y4B",
+      url: "https://cdn2.thecatapi.com/images/2JgKj4y4B.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "35ayLP9Ql",
+      url: "https://cdn2.thecatapi.com/images/35ayLP9Ql.jpg",
+      name: "Somali / 소말리 고양이",
+    },
+    {
+      id: "8B1WVLAWH",
+      url: "https://cdn2.thecatapi.com/images/8B1WVLAWH.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "wJyw82pIl",
+      url: "https://cdn2.thecatapi.com/images/wJyw82pIl.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+    {
+      id: "QUdOiX2hP",
+      url: "https://cdn2.thecatapi.com/images/QUdOiX2hP.jpg",
+      name: "Norwegian Forest Cat / 노르웨이 숲 고양이",
+    },
+  ],
+};
+
 console.log("app is running");
 
 class App {
@@ -9,14 +174,22 @@ class App {
     this.searchInput = new SearchInput({
       $target,
       onSearch: (keyword) => {
-        api.fetchCats(keyword).then(({ data }) => this.setState(data));
+        // api.fetchCats(keyword).then(({ data }) => this.setState(data));
+        this.setState(apiData.data);
+      },
+    });
+
+    this.searchResult = new SearchResult({
+      $target,
+      initialData: this.data,
+      onClick: (image) => {
+        console.log(image);
       },
     });
   }
 
   setState(nextData) {
-    console.log(this);
     this.data = nextData;
-    console.log(this.data);
+    this.searchResult.setState(nextData);
   }
 }
