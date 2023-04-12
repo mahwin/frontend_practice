1162048a2ef8aea414d7cbf0e73dd4f2205a3453
youseock <mahwin7085@gmail.com>
2023-04-12 20:10:26 +0900

    Refactory : 함수형 컴포넌트로 변경

--- 과제테스트연습/고양이검색사이트/src/components/ImageInfo.js
+++ 과제테스트연습/고양이검색사이트/src/components/ImageInfo.js
@@ -1,60 +1,31 @@
-class ImageInfo {
-  $imageInfo = null;
-  data = null;
+export default function ImageInfo({ $app, initialState }) {
+  this.state = initialState;
+  this.$target = document.createElement("div");
+  this.$target.className = "ImageInfo";
+  $app.appendChild(this.$target);
 
-  constructor({ $target, data }) {
-    const $imageInfo = document.createElement("div");
-    $imageInfo.className = "ImageInfo";
-    this.$imageInfo = $imageInfo;
-    $target.appendChild($imageInfo);
-
-    this.data = data;
-
+  this.setState((newState) => {
+    this.state = newState;
     this.render();
-  }
+  });
 
-  setState(nextData) {
-    this.data = nextData;
-    this.render();
-  }
-
-  render() {
-    if (this.data.visible) {
-      this.$imageInfo.classList.remove("fade");
-      const { name, url, temperament, origin } = this.data.image;
-
-      this.$imageInfo.innerHTML = `
-        <div class="content-wrapper">
-          <div class="title">
-            <span>${name}</span>
-            <div class="close">x</div>
-          </div>
-          <img src="${url}" alt="${name}"/>        
-          <div class="description">
-            <div>성격: ${temperament}</div>
-            <div>태생: ${origin}</div>
-          </div>
-        </div>`;
-      this.$imageInfo.style.display = "block";
-
-      document.addEventListener("click", (e) => {
-        if (
-          e.target === document.querySelector(".ImageInfo") ||
-          e.target === document.querySelector(".close")
-        ) {
-          this.$imageInfo.style.display = "none";
-          this.$imageInfo.classList.add("fade");
-        }
-      });
-
-      document.addEventListener("keydown", (e) => {
-        if (e.code === "Escape") {
-          this.$imageInfo.style.display = "none";
-          this.$imageInfo.classList.add("fade");
-        }
-      });
-    } else {
-      this.$imageInfo.style.display = "none";
+  this.render = () => {
+    if (this.state.image) {
+      const { name, url, temperament, origin } = this.state.image;
+      this.$target.innerHTML = `
+      <div class="content-wrapper">
+        <div class="title">
+          <span>${name}</span>
+          <div class="close"</div>
+        </div>
+        <img src=${url} alt=${name}/>
+        <div class="description">
+          <div>성격 : ${temperament}</div>
+          <div>태생 : ${origin}</div>
+        </div>
+      </div>`;
     }
-  }
+    this.$target.style.display = this.state.visible ? "block" : "none";
+  };
+  this.render();
 }
