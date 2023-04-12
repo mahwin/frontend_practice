920a25a1dda9b7fa9e5c0b6905486a88316861e2
youseock <mahwin7085@gmail.com>
2023-04-12 20:13:14 +0900

    Add : 페이지 진입 시 검색창으로 focus

--- 과제테스트연습/고양이검색사이트/src/components/SearchInput.js
+++ 과제테스트연습/고양이검색사이트/src/components/SearchInput.js
@@ -3,6 +3,7 @@ export default function SearchInput({ $app, onSearch }) {
   this.$target.className = "SearchInput";
   this.$target.placeholder = "고양이를 검색해보세요.|";
   $app.appendChild(this.$target);
+  this.$target.focus();
 
   this.onSearch = onSearch;
   this.$target.addEventListener("keyup", (e) => {
