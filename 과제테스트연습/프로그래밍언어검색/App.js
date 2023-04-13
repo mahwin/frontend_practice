d8936189fdfc8e96e0f8935f4e8126cd0c4f20aa
youseock <mahwin7085@gmail.com>
2023-04-13 20:20:09 +0900

    Add : input 요소로 입력 받은 데이터 api로 통신

--- 과제테스트연습/프로그래밍언어검색/App.js
+++ 과제테스트연습/프로그래밍언어검색/App.js
@@ -1,4 +1,5 @@
 import SearchInput from "./SearchInput.js";
+import { fetchLanguages } from "./api.js";
 
 export default function App({ $target }) {
   this.state = {
@@ -10,5 +11,11 @@ export default function App({ $target }) {
     //TODO
   };
 
-  const searchInput = new SearchInput({ $target, initialState: "" });
+  const searchInput = new SearchInput({
+    $target,
+    initialState: "",
+    onChange: async (keyword) => {
+      const languages = await fetchLanguages(keyword);
+    },
+  });
 }
--- 과제테스트연습/프로그래밍언어검색/SearchInput.js
+++ 과제테스트연습/프로그래밍언어검색/SearchInput.js
@@ -1,4 +1,4 @@
-export default function SearchInput({ $target, initialState }) {
+export default function SearchInput({ $target, initialState, onChange }) {
   this.$element = document.createElement("form");
   this.$element.className = "SearchInput";
   this.state = initialState;
@@ -9,6 +9,10 @@ export default function SearchInput({ $target, initialState }) {
     this.$element.innerHTML = `
     <input class="SearchInput__input" type="text" placeholder="프로그램 언어를 입력하세요." value="${this.state}">`;
   };
+
+  this.$element.addEventListener("keyup", (e) => {
+    onChange(e.target.value);
+  });
 
   this.render();
 }
