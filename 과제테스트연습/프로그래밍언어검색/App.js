ebcb6eff225f70197dd7d41a358a82e26bb6d9c1
youseock <mahwin7085@gmail.com>
2023-04-13 20:17:04 +0900

    Add : 입력 컴포넌트

---
+++ 과제테스트연습/프로그래밍언어검색/App.js
@@ -0,0 +1,14 @@
+import SearchInput from "./SearchInput.js";
+
+export default function App({ $target }) {
+  this.state = {
+    fetchedLanguages: [],
+    selectedLanguages: [],
+  };
+
+  this.setState = (nextState) => {
+    //TODO
+  };
+
+  const searchInput = new SearchInput({ $target, initialState: "" });
+}
