2867dcf76489f353d07644b82ec9419d8a209b6e
youseock <mahwin7085@gmail.com>
2023-04-17 19:17:19 +0900

    Add : 헤더

---
+++ 과제테스트연습/인사정보리뉴얼/web/src/componenets/Header.js
@@ -0,0 +1,40 @@
+class Header {
+  constructor($body) {
+    this.$body = $body;
+  }
+
+  createMenuElem(divClass, spanClass, spanId, menuText) {
+    const div = document.createElement("div");
+    div.setAttribute("class", divClass);
+
+    const span = document.createAttribute("span");
+    span.setAttribute("class", spanClass);
+    span.setAttribute("id", spanId);
+    span.appendChild(document.createTextNode(menuText));
+    div.appendChild(span);
+
+    return div;
+  }
+
+  return() {
+    const header = document.createElement("header");
+    const home_menu = this.createMenuElem(
+      "header header_left",
+      "menu_name",
+      "menu_home",
+      "HOME"
+    );
+    const signup_menu = this.createMenuElem(
+      "header header_right",
+      "menu_name",
+      "menu_signup",
+      "SIGNUP"
+    );
+
+    header.appendChild(home_menu);
+    header.appendChild(signup_menu);
+    this.$body.appendChild(header);
+  }
+}
+
+export default Header;
