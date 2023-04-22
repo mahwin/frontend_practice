a396959b5d34dfd9fe73316670eb95f067e560b3
youseock <mahwin7085@gmail.com>
2023-04-22 22:10:56 +0900

    feat : Api

--- 과제테스트연습/고양이검색사이트/src/api.js
+++ 과제테스트연습/고양이검색사이트/src/api.js
@@ -1,10 +1,23 @@
 const API_ENDPOINT =
-  "https://q9d70f82kd.execute-api.ap-northeast-2.amazonaws.com/dev";
+  "https://oivhcpn8r9.execute-api.ap-northeast-2.amazonaws.com/dev";
 
-const api = {
+const request = async (url) => {
+  const res = await fetch(url);
+  return await res.json();
+};
+
+export const api = {
   fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
-      res.json()
-    );
+    return request(`${API_ENDPOINT}/api/cats/search?q=${keyword}`);
+  },
+
+  fetchCatDetail: (id) => {
+    return request(`${API_ENDPOINT}/api/cats/${id}`);
+  },
+
+  fetchRandomCats: () => {
+    return request(`${API_ENDPOINT}/api/cats/random50`);
   },
 };
+
+// const request = async (url) => {     try {       const result = await fetch(url);       return result.json();     } catch (e) {       console.warn(e);     }   }    const api = {     fetchGif: keyword => {       return request(`${API_ENDPOINT}/api/gif/search?q=${keyword}`);     },     fetchGifAll: () => {       return request(`${API_ENDPOINT}/api/gif/all`);     }   };
