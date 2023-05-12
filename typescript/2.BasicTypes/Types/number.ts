23d0b8a889da740298e3fe7bcac932645bfd60d8
youseock <mahwin7085@gmail.com>
2023-05-13 00:21:48 +0900

    number

---
+++ typescript/2.BasicTypes/Types/number.ts
@@ -0,0 +1,13 @@
+//타입 스크립도 자바 스크립트와 마찬가지로 모든 수가 부동 소수점 값이다.
+
+let decimal: number = 6;
+
+let hex: number = 0xf00d; // 0x가 16진수를 의미함
+
+let binary: number = 0b1010; // 0b가 2진수를 의미함
+
+let octal: number = 0o2323; //  0o가 8진수를 의미함
+
+let notANumber: number = NaN; // NaN는 숫자의 한 형태임
+
+let unerscoreNum: number = 100_200_300;
