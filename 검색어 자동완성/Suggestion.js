518d29d7d2f09f639532fbf64e7e9250c8fa59e8
youseock <mahwin7085@gmail.com>
2023-06-30 20:14:59 +0900

    Suggestion 컴포넌트에 키보드 이벤트 추가

--- 검색어 자동완성/Suggestion.js
+++ 검색어 자동완성/Suggestion.js
@@ -6,25 +6,65 @@ export default function Suggestion({ target, searchResult }) {
 
   this.state = {
     searchResult: searchResult,
+    selectedIndex: 0,
   };
 
   this.setState = (newState) => {
     this.state = {
+      ...this.state,
       ...newState,
     };
-    console.log(newState);
+    console.log(this.state);
     this.render();
   };
 
   this.render = () => {
     const infos = this.state.searchResult;
+    const selectedIndex = this.state.selectedIndex;
+    console.log(selectedIndex);
     if (infos.length) {
       this.element.innerHTML = `${infos
-        .map((info) => `<li>${info.word}</li>`)
+        .map(
+          (info, i) =>
+            `${
+              i === selectedIndex
+                ? `<li class="selected">${info.word}</li>`
+                : `<li>${info.word}</li>`
+            }`
+        )
         .join("")}`;
       this.element.style.display = "block";
     } else {
       this.element.style.display = "none";
     }
   };
+
+  window.addEventListener("keyup", (e) => {
+    const activateKey = ["ArrowUp", "ArrowDown", "Enter"];
+    const lastIndex = this.state.searchResult.length - 1;
+    console.log(lastIndex);
+    if (activateKey.includes(e.key)) {
+      switch (e.key) {
+        case "ArrowUp":
+          this.setState({
+            selectedIndex:
+              this.state.selectedIndex === 0
+                ? lastIndex
+                : this.state.selectedIndex - 1,
+          });
+          break;
+        case "ArrowDown":
+          this.setState({
+            selectedIndex:
+              this.state.selectedIndex === lastIndex
+                ? 0
+                : this.state.selectedIndex + 1,
+          });
+          break;
+        case "Enter":
+          selectLanguage(this.state.searchResult[this.state.selectedIndex]);
+          break;
+      }
+    }
+  });
 }
